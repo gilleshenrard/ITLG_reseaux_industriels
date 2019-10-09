3d2b52d78802fdd0d0f1fa36915df5721851b81b
Gilles Henrard <gilles.henrard@hotmail.com>
2019-10-09 12:06:02 +0200

    improve server code and add comments

--- server.c
+++ server.c
@@ -1,17 +1,17 @@
 /*
-** server.c -- a stream socket server demo
+** server.c
+** Waits for clients to connect via a stream socket, and sends them a 'Hello World' reply
+** Made by Brian 'Beej Jorgensen' Hall
+** Modified by Gilles Henrard
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-//#include <sys/types.h>
-//#include <sys/socket.h>
-//#include <netinet/in.h>
 #include <netdb.h>
 #include <sys/wait.h>
-//#include <signal.h>
 #include "network.h"
 
 #define PORT "3490" // the port users will be connecting to
@@ -19,7 +19,7 @@
 
 void sigchld_handler(/*int s*/);
 
-int main(void)
+int main(/*int argc, char *argv[]*/)
 {
 	int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
 	struct addrinfo hints, *servinfo, *p;
@@ -29,11 +29,15 @@ int main(void)
 	int yes=1;
 	char s[INET6_ADDRSTRLEN];
 	int rv;
+
+	//prepare the structure holding socket information
+	// any potocol, stream socket, local IP, rest to 0
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE; // use my IP
 
+	//format socket information and store it in list servinfo
 	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
 	{
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
@@ -43,20 +47,22 @@ int main(void)
 // loop through all the results and bind to the first we can
 	for (p = servinfo; p != NULL; p = p->ai_next)
 	{
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				     p->ai_protocol)) == -1)
+        //generate a socket file descriptor
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
 		{
 			perror("server: socket");
 			continue;
 		}
 
+		//allow reconnections on the socket if still allocated in kernel
 		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
 				sizeof(int)) == -1)
 		{
 			perror("setsockopt");
-			exit(1);
+			exit(EXIT_FAILURE);
 		}
 
+		//bind the socket to the desired port (useful in a server)
 		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
 		{
 			close(sockfd);
@@ -67,63 +73,76 @@ int main(void)
 		break;
 	}
 
-	freeaddrinfo(servinfo); // all done with this structure
+	// no need for the serv structure list anymore
+	freeaddrinfo(servinfo);
 
+	//no socket available
 	if (p == NULL)
 	{
 		fprintf(stderr, "server: failed to bind\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
+	//listen to socket created
 	if (listen(sockfd, BACKLOG) == -1)
 	{
 		perror("listen");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
-	sa.sa_handler = sigchld_handler; // reap all dead processes
+	//prepare main process for SIGCHLD signals
+	sa.sa_handler = sigchld_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
-
 	if (sigaction(SIGCHLD, &sa, NULL) == -1)
 	{
 		perror("sigaction");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	printf("server: waiting for connections...\n");
 
-	while (1)  // main accept() loop
+	// main accept() loop
+	while(1)
 	{
+        //wait for client to request a connection + create connection socket accordingly
 		sin_size = sizeof their_addr;
-		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-
-		if (new_fd == -1)
+		if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) == -1)
 		{
 			perror("accept");
 			continue;
 		}
 
+		//translate IPv4 and IPv6 on the fly depending on the client request
 		inet_ntop(their_addr.ss_family,
 			  get_in_addr((struct sockaddr *)&their_addr),
 			  s, sizeof s);
 		printf("server: got connection from %s\n", s);
 
-		if (!fork())   // this is the child process
-		{
-			close(sockfd); // child doesn't need the listener
+		switch(fork()){
+            case -1: //fork error
+                perror("fork");
+                break;
 
-			if (send(new_fd, "Hello, world!", 13, 0) == -1)
-				perror("send");
+            case 0: //child process
+                close(sockfd); // child doesn't need the listener
 
-			close(new_fd);
-			exit(0);
+                //send message to child
+                if (send(new_fd, "Hello, world!", 13, 0) == -1)
+                    perror("send");
+
+                //close connection socket and exit child process
+                close(new_fd);
+                exit(EXIT_SUCCESS);
+                break;
+
+            default: //parent process
+                close(new_fd); // parent doesn't need this
+                break;
 		}
-
-		close(new_fd); // parent doesn't need this
 	}
 
-	return 0;
+	exit(EXIT_SUCCESS);
 }
 
 /************************************************************************/
@@ -136,6 +155,7 @@ void sigchld_handler(/*int s*/)
     // waitpid() might overwrite errno, so we save and restore it:
 	int saved_errno = errno;
 
+	printf("Received a SIGCHLD signal...\n");
 	while (waitpid(-1, NULL, WNOHANG) > 0);
 
 	errno = saved_errno;
