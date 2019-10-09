b9daa760c39dc16470cf3af7659f442c432cbe48
Gilles Henrard <gilles.henrard@hotmail.com>
2019-10-09 21:28:47 +0200

    make client use the new negociation function

--- client.c
+++ client.c
@@ -7,11 +7,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <arpa/inet.h>
 #include <netdb.h>
-#include <sys/wait.h>
 #include "network.h"
 
 #define PORT "3490" // the port client will be connecting to
@@ -21,9 +20,8 @@ int main(int argc, char *argv[])
 {
 	int sockfd, numbytes;
 	char buf[MAXDATASIZE];
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	char s[INET6_ADDRSTRLEN];
+	int ret = 0;
+//	char s[INET6_ADDRSTRLEN];
 
 	//checks if the port number has been provided
 	if (argc != 2)
@@ -32,56 +30,14 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-    //prepare the structure holding socket information
-	// any potocol, stream socket, hostname set as argument, rest to 0
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-
-	//format socket information and store it in list servinfo
-	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0)
-	{
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-
-    // loop through all the results and connect to the first we can
-	for (p = servinfo; p != NULL; p = p->ai_next)
-	{
-        //generate a socket file descriptor
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				     p->ai_protocol)) == -1)
-		{
-			perror("client: socket");
-			continue;
-		}
-
-		//connect to the server via the socket created
-		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
-		{
-			close(sockfd);
-			perror("client: connect");
-			continue;
-		}
-
-		break;
-	}
-
-	//no socket available
-	if (p == NULL)
-	{
-		fprintf(stderr, "client: failed to connect\n");
-		return 2;
-	}
-
+    ret = negociate_socket(argv[1], PORT, &sockfd, CONNECT);
+    printf("%d\n", ret);
+/*
 	//translate IPv4 and IPv6 on the fly depending on the client request
 	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
 		  s, sizeof s);
 	printf("client: connecting to %s\n", s);
-
-	// all done with this structure
-	freeaddrinfo(servinfo);
-
+*/
 	//receive message from the server
 	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1)
 	{
