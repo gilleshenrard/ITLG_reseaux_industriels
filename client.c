61627694d81dc2c34fbf2efa328253dd3bbeee4f
Gilles Henrard <gilles.henrard@hotmail.com>
2019-10-13 19:45:50 +0200

    add comment to explain why connect UDP socket

--- client.c
+++ client.c
@@ -41,6 +41,8 @@ int main(int argc, char *argv[])
     }
 
     //create the actual socket
+    //if UDP is chosen, socket will be a connected datagram socket
+    //  see pg 32 of Beej's book
     ret = negociate_socket(servinfo, &sockfd, CONNECT);
     if(ret != 0){
         fprintf(stderr, "client: could not create a socket\n");
