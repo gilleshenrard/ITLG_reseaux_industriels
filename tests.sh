24bcba773999b7acadba84419af669c18b62ad7d
Gilles Henrard <gilles.henrard@hotmail.com>
2019-10-28 16:21:03 +0100

    change colours for tests title

--- tests.sh
+++ tests.sh
@@ -2,46 +2,51 @@
 
 cd bin/Debug
 
-echo '0- test of the client application with an improper number of arguments'
-echo './client clapton'
+echo -e '\e[1m0- test of the client application with an improper number of arguments\e[0m'
+echo -e '\e[1m./client clapton\e[0m'
 ./client clapton
 
 echo ''
-echo '1- test of the server application via hostname, TCP, port 13'
-echo './client clapton 13 tcp "this is a test"'
-./client clapton 13 tcp "this is a test"
+echo -e '\e[1m1- test of the server application via hostname, TCP, port 7\e[0m'
+echo -e '\e[1m./client clapton 7 tcp "this is a test"\e[0m'
+./client clapton 7 tcp "this is a test"
 
 echo ''
-echo '2- test of the server application via direct IP, TCP, port 13'
-echo './client 192.168.56.10 13 tcp "this is a test"'
-./client 192.168.56.10 13 tcp "this is a test"
+echo -e '\e[1m2- test of the server application via direct IP, TCP, port 7\e[0m'
+echo -e '\e[1m./client 192.168.56.10 7 tcp "this is a test"\e[0m'
+./client 192.168.56.10 7 tcp "this is a test"
 
 echo ''
-echo '3- test of the server application via hostname, UDP, port 13'
-echo './client vaughan 13 udp "this is a test"'
-./client vaughan 13 udp "this is a test"
+echo -e '\e[1m3- test of the server application via hostname, TCP, protocol name\e[0m'
+echo -e '\e[1m./client clapton "echo" tcp "this is a test"\e[0m'
+./client clapton "echo" tcp "this is a test"
 
 echo ''
-echo '4- test of the server application via direct IP, UDP, port 13'
-echo './client 192.168.56.11 13 udp "this is a test"'
-./client 192.168.56.11 13 udp "this is a test"
+echo -e '\e[1m4- test of the server application via hostname, UDP, port 7\e[0m'
+echo -e '\e[1m./client vaughan 7 udp "this is a test"\e[0m'
+./client vaughan 7 udp "this is a test"
 
 echo ''
-echo '5- test of a non-existing hostname'
-echo './client dummy 13 tcp "this is a test"'
-./client dummy 13 tcp "this is a test"
+echo -e '\e[1m5- test of the server application via direct IP, UDP, port 7\e[0m'
+echo -e '\e[1m./client 192.168.56.11 7 udp "this is a test"\e[0m'
+./client 192.168.56.11 7 udp "this is a test"
 
 echo ''
-echo '6- test of a wrong IP'
-echo './client 456.123.1.468 13 tcp "this is a test"'
-./client 456.123.1.468 13 tcp "this is a test"
+echo -e '\e[1m6- test of a non-existing hostname\e[0m'
+echo -e '\e[1m./client dummy 7 tcp "this is a test"\e[0m'
+./client dummy 7 tcp "this is a test"
 
 echo ''
-echo '7- test of a wrong port (will timeout after 5 sec.)'
-echo './client clapton 79456 tcp "this is a test"'
+echo -e '\e[1m7- test of a wrong IP\e[0m'
+echo -e '\e[1m./client 456.123.1.468 7 tcp "this is a test"\e[0m'
+./client 456.123.1.468 7 tcp "this is a test"
+
+echo ''
+echo -e '\e[1m8- test of a wrong port (will timeout after 5 sec.)\e[0m'
+echo -e '\e[1m./client clapton 79456 tcp "this is a test"\e[0m'
 ./client clapton 79456 tcp "this is a test"
 
 echo ''
-echo '8- test of a port blocked by firewall (will timeout after 5 sec.)'
-echo './client clapton 80 tcp "this is a test"'
+echo -e '\e[1m9- test of a port blocked by firewall (will timeout after 5 sec.)\e[0m'
+echo -e '\e[1m./client clapton 80 tcp "this is a test"\e[0m'
 ./client clapton 80 tcp "this is a test"
