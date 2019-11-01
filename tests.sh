996507d27f83ecef0b51f6ba3a1ae89d78509108
Gilles Henrard <gilles.henrard@hotmail.com>
2019-11-01 11:58:35 +0100

    add tests for stderr and stdout

--- tests.sh
+++ tests.sh
@@ -3,7 +3,7 @@
 #	clapton : 192.168.56.10, port 7, tcp
 #	vaughan : 192.168.56.11, port 7, udp
 # Made by Gilles Henrard
-# Last modification : 30/10/2019
+# Last modification : 01/11/2019
 
 #! /bin/bash
 
@@ -58,11 +58,23 @@ echo -e '\e[1m./client 456.123.1.468 7 tcp "this is the test 7"\e[0m'
 
 #test 8
 echo ''
+echo -e '\e[1m7- test with stderr ignored\e[0m'
+echo -e '\e[1m./client 456.123.1.468 7 tcp "this is the test 7" 2> /dev/null\e[0m'
+./client 456.123.1.468 7 tcp "this is the test 7" 2> /dev/null
+
+#test 9
+echo ''
+echo -e '\e[1m1- test with stdout ignored\e[0m'
+echo -e '\e[1m./client clapton 7 tcp "this is the test 1" 1> /dev/null\e[0m'
+./client clapton 7 tcp "this is the test 9" 1> /dev/null
+
+#test 10
+echo ''
 echo -e '\e[1m8- test of a wrong port (will timeout after 5 sec.)\e[0m'
 echo -e '\e[1m./client clapton 79456 tcp "this is the test 8"\e[0m'
 ./client clapton 79456 tcp "this is the test 8"
 
-#test 9
+#test 11
 echo ''
 echo -e '\e[1m9- test of a port blocked by firewall (will timeout after 5 sec.)\e[0m'
 echo -e '\e[1m./client clapton 80 tcp "this is the test 9"\e[0m'
