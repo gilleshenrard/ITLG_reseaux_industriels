67fae5693d854db0fa8a9ac0c2b8737d514a8a3a
Gilles Henrard <gilles.henrard@hotmail.com>
2019-11-01 11:45:04 +0100

    update last change date

--- client.c
+++ client.c
@@ -4,7 +4,7 @@
 ** -------------------------------------------
 ** Based on Brian 'Beej Jorgensen' Hall's code
 ** Made by Gilles Henrard
-** Last modified : 30/10/2019
+** Last modified : 01/11/2019
 */
 
 #include "global.h"
