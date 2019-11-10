743d474c0f5b96a2ccc5e189f26dee1ff40476d6
Gilles Henrard <gilles.henrard@hotmail.com>
2019-11-10 16:06:15 +0100

    fix issues with dataset declaration

--- include/dataset.h
+++ include/dataset.h
@@ -1,10 +1,11 @@
 #ifndef DATASET_H_INCLUDED
 #define DATASET_H_INCLUDED
+#include <stdlib.h>
 
 typedef struct{
-    int32_t     amount;
-    char[32]    type;
-    float       price;
+    int32_t amount;
+    char    type[32];
+    float   price;
 }dataset_t;
 
 #endif // DATASET_H_INCLUDED
