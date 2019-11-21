ded644a69eab7231f449d3cfbb2d808ff2622398
Gilles Henrard <gilles.henrard@hotmail.com>
2019-11-21 22:00:10 +0100

    implement b-tree files functions

--- include/algo.h
+++ include/algo.h
@@ -73,8 +73,8 @@ dyndata_t* min_AVL_value(meta_t* meta, dyndata_t* avl);
 int delete_AVL_root(meta_t* meta);
 
 ////File binary trees
-//long index_tree(FILE* fp, long offset_start, long nb, t_algo_meta* meta);
-//int searchall_index(FILE* fp, long offset_root, void* key, t_algo_meta* index, t_algo_meta* list, int elem_size);
-//int searchone_index(FILE* fp, long offset_root, void* key, t_algo_meta* index, void* element, int elem_size);
+long index_tree(FILE* fp, long offset_start, long nb, meta_t* meta);
+int searchall_index(FILE* fp, long offset_root, void* key, meta_t* index, meta_t* lis, int elem_size);
+int searchone_index(FILE* fp, long offset_root, void* key, meta_t* index, void* element, int elem_size);
 
 #endif // ALGO_H_INCLUDED
