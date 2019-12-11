cf1ea74c64a4435c8a3878f8a6a53213b9590f7c
Gilles Henrard <gilles.henrard@hotmail.com>
2019-12-11 22:19:59 +0100

    optimise ldconfig

--- lib/build.mk
+++ lib/build.mk
@@ -20,37 +20,37 @@ lib_b:= libscreen.so libnetwork.so libdataset.so libalgo.so libserialisation.so 
 libscreen.so : ../src/screen.o
 	@ echo "Building $@"
 	$(CC) -shared -fPIC -lc -Wl,-soname,$@.1 -o $@.1.0 $<
-	ldconfig -l $@.1.0
+	ldconfig -n . -l $@.1.0
 	ln -sf $@.1 $@
 
 libnetwork.so : ../src/network.o
 	@ echo "Building $@"
 	$(CC) -shared -fPIC -lc -Wl,-soname,$@.2 -o $@.2.0 $<
-	ldconfig -l $@.2.0
+	ldconfig -n . -l $@.2.0
 	ln -sf $@.2 $@
 
 libalgo.so : ../src/algo.o
 	@ echo "Building $@"
 	$(CC) -shared -fPIC -lc -Wl,-soname,$@.2 -o $@.2.0 $<
-	ldconfig -l $@.2.0
+	ldconfig -n . -l $@.2.0
 	ln -sf $@.2 $@
 
 libdataset.so : ../src/dataset.o
 	@ echo "Building $@"
 	$(CC) -shared -fPIC -lc -Wl,-soname,$@.1 -o $@.1.2 $<
-	ldconfig -l $@.1.2
+	ldconfig -n . -l $@.1.2
 	ln -sf $@.1 $@
 
 libserialisation.so : ../src/serialisation.o
 	@ echo "Building $@"
 	$(CC) -shared -fPIC -lc -Wl,-soname,$@.1 -o $@.1.1 $<
-	ldconfig -l $@.1.1
+	ldconfig -n . -l $@.1.1
 	ln -sf $@.1 $@
 
 libprotocol.so : ../src/protocol.o libalgo.so libnetwork.so libserialisation.so
 	@ echo "Building $@"
 	$(CC) -shared -fPIC -lc -L. -Wl,-soname,$@.1 -o $@.1.0 $< -lalgo -lnetwork -lserialisation
-	ldconfig -l $@.1.0
+	ldconfig -n . -l $@.1.0
 	ln -sf $@.1 $@
 
 
