18b8cfb5589e0e80ab571a640c6f6199fdad06e1
Gilles Henrard <gilles.henrard@hotmail.com>
2020-01-11 12:45:44 +0100

    change unit tests order

--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
 # This script tests all the possible outcomes between the client and the server
 # Made by Gilles Henrard
-# Last modification : 19/11/2019
+# Last modification : 11/01/2020
 
 # Set up
 # The assignment is programmed in C98, compiled with GNU GCC.
@@ -19,55 +19,57 @@
 
 #! /bin/bash
 
-#test 0
-echo -e '\e[1m0- test of the client application with an improper number of arguments\e[0m'
+#test 1
+echo -e '\e[1m1- test of the client application with an improper number of arguments\e[0m'
 echo -e '\e[1mbin/client clapton\e[0m'
 bin/client clapton
 
-#test 1
-echo ''
-echo -e '\e[1m1- test of the server application via hostname\e[0m'
-echo -e '\e[1mbin/client clapton 3490\e[0m'
-bin/client clapton 3490
-
 #test 2
 echo ''
-echo -e '\e[1m2- test of the server application via direct IP\e[0m'
-echo -e '\e[1mbin/client 192.168.56.10 3490\e[0m'
-bin/client 192.168.56.10 3490
-
-#test 3
-echo ''
-echo -e '\e[1m3- test of a non-existing hostname\e[0m'
+echo -e '\e[1m2- test of a non-existing hostname\e[0m'
 echo -e '\e[1mbin/client dummy 3490\e[0m'
 bin/client dummy 3490
 
-#test 4
+#test 3
 echo ''
-echo -e '\e[1m4- test of a wrong IP\e[0m'
+echo -e '\e[1m3- test of a wrong IP\e[0m'
 echo -e '\e[1mbin/client 456.123.1.468 3490\e[0m'
 bin/client 456.123.1.468 3490
 
-#test 5
+#test 4
 echo ''
-echo -e '\e[1m5- test with stderr ignored\e[0m'
+echo -e '\e[1m4- test with stderr ignored\e[0m'
 echo -e '\e[1mbin/client 456.123.1.468 3490 2> /dev/null\e[0m'
 bin/client 456.123.1.468 3490 2> /dev/null
 
-#test 6
+#test 5
 echo ''
-echo -e '\e[1m6- test with stdout ignored\e[0m'
+echo -e '\e[1m5- test with stdout ignored\e[0m'
 echo -e '\e[1mbin/client clapton 3490 1> /dev/null\e[0m'
 bin/client clapton 3490 1> /dev/null
 
-#test 7
+#test 6
 echo ''
-echo -e '\e[1m7- test of a wrong port (will timeout after 5 sec.)\e[0m'
+echo -e '\e[1m6- test of a wrong port (will timeout after 5 sec.)\e[0m'
 echo -e '\e[1mbin/client clapton 79456\e[0m'
 bin/client clapton 79456
 
-#test 8
+#test 7
 echo ''
-echo -e '\e[1m8- test of a port blocked by firewall (will timeout after 5 sec.)\e[0m'
+echo -e '\e[1m7- test of a port blocked by firewall (will timeout after 5 sec.)\e[0m'
 echo -e '\e[1mbin/client clapton 80\e[0m'
 bin/client clapton 80
+
+#test 8
+echo ''
+echo -e '\e[1m8- test of the server application via hostname\e[0m'
+echo -e '\e[1mbin/client clapton 3490\e[0m'
+bin/client clapton 3490
+
+#test 9
+echo ''
+echo -e '\e[1m9- test of the server application via direct IP\e[0m'
+echo -e '\e[1mbin/client 192.168.56.10 3490\e[0m'
+bin/client 192.168.56.10 3490
+
+
