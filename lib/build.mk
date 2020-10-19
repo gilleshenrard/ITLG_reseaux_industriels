f9b30459b54e9d3966c8aaf569458d0b8a814c2f
Gilles Henrard <gilles.henrard@hotmail.com>
2020-10-19 17:36:06 +0200

    make lib/build.mk less verbose

--- lib/build.mk
+++ lib/build.mk
@@ -13,51 +13,51 @@ lib_b:= libscreen.so libnetwork.so libdataset.so libalgo.so libserialisation.so 
 #objects compilation from the source files
 %.o: %.c
 	@ echo "Building $@"
-	$(CC) $(CFLAGS) -o $@ -c $<
+	@ $(CC) $(CFLAGS) -o $@ -c $<
 
 
 #libraries compilation and linking (version number -> *.so file)
 libscreen.so : ../src/screen.o
 	@ echo "Building $@"
-	$(CC) -shared -fPIC -lc -Wl,-soname,$@.1 -o $@.1.0 $<
-	ldconfig -n . -l $@.1.0
-	ln -sf $@.1 $@
+	@ $(CC) -shared -fPIC -lc -Wl,-soname,$@.1 -o $@.1.0 $<
+	@ ldconfig -n . -l $@.1.0
+	@ ln -sf $@.1 $@
 
 libnetwork.so : ../src/network.o
 	@ echo "Building $@"
-	$(CC) -shared -fPIC -lc -Wl,-soname,$@.2 -o $@.2.0 $<
-	ldconfig -n . -l $@.2.0
-	ln -sf $@.2 $@
+	@ $(CC) -shared -fPIC -lc -Wl,-soname,$@.2 -o $@.2.0 $<
+	@ ldconfig -n . -l $@.2.0
+	@ ln -sf $@.2 $@
 
 libalgo.so : ../src/algo.o
 	@ echo "Building $@"
-	$(CC) -shared -fPIC -lc -Wl,-soname,$@.2 -o $@.2.1 $<
-	ldconfig -n . -l $@.2.1
-	ln -sf $@.2 $@
+	@ $(CC) -shared -fPIC -lc -Wl,-soname,$@.2 -o $@.2.1 $<
+	@ ldconfig -n . -l $@.2.1
+	@ ln -sf $@.2 $@
 
 libdataset.so : ../src/dataset.o
 	@ echo "Building $@"
-	$(CC) -shared -fPIC -lc -Wl,-soname,$@.1 -o $@.1.2 $<
-	ldconfig -n . -l $@.1.2
-	ln -sf $@.1 $@
+	@ $(CC) -shared -fPIC -lc -Wl,-soname,$@.1 -o $@.1.2 $<
+	@ ldconfig -n . -l $@.1.2
+	@ ln -sf $@.1 $@
 
 libdataset_test.so : ../src/dataset_test.o
 	@ echo "Building $@"
-	$(CC) -shared -fPIC -lc -Wl,-soname,$@.1 -o $@.1.0 $<
-	ldconfig -n . -l $@.1.0
-	ln -sf $@.1 $@
+	@ $(CC) -shared -fPIC -lc -Wl,-soname,$@.1 -o $@.1.0 $<
+	@ ldconfig -n . -l $@.1.0
+	@ ln -sf $@.1 $@
 
 libserialisation.so : ../src/serialisation.o
 	@ echo "Building $@"
-	$(CC) -shared -fPIC -lc -Wl,-soname,$@.1 -o $@.1.1 $<
-	ldconfig -n . -l $@.1.1
-	ln -sf $@.1 $@
+	@ $(CC) -shared -fPIC -lc -Wl,-soname,$@.1 -o $@.1.1 $<
+	@ ldconfig -n . -l $@.1.1
+	@ ln -sf $@.1 $@
 
 libprotocol.so : ../src/protocol.o libalgo.so libnetwork.so libserialisation.so
 	@ echo "Building $@"
-	$(CC) -shared -fPIC -lc -L. -Wl,-soname,$@.2 -o $@.2.0 $< -lalgo -lnetwork -lserialisation
-	ldconfig -n . -l $@.2.0
-	ln -sf $@.2 $@
+	@ $(CC) -shared -fPIC -lc -L. -Wl,-soname,$@.2 -o $@.2.0 $< -lalgo -lnetwork -lserialisation
+	@ ldconfig -n . -l $@.2.0
+	@ ln -sf $@.2 $@
 
 
 #overall functions
